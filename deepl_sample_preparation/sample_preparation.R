f45201a0b94d72b976f84ec577ac17cb85c66b96
loreabad6 <lore.abad6@gmail.com>
2021-03-16 16:12:18 +0100

    Update sample prep

--- deepl_sample_preparation/sample_preparation.R
+++ deepl_sample_preparation/sample_preparation.R
@@ -1,5 +1,5 @@
 #' ---
-#' title: "Vector sample preparation for DeepL analysis"
+#' title: "Vector sample preparation for Deep Learning analysis"
 #' subtitle: "STEC project"
 #' author: "Lorena Abad"
 #' date: "March 8, 2021"
@@ -22,51 +22,68 @@ library(sf)
 library(tidyverse, quietly = T, warn.conflicts = F)
 
 #+ Directories
-stec_dir = "Data_from_R"
+stec_dir = "R:/RESEARCH/02_PROJECTS/01_P_330001/119_STEC/04_Data/Gullies-Mangatu"
+
+#+ Data
+g39 = st_read(here(stec_dir, 'mangatu_coregistered_LA', 'mangatu_1939_ero_feat_nztm.shp'))
+g57 = st_read(here(stec_dir, 'Mangatu_feature_extraction', 'gullies_57_tm.shp'))
+g60 = st_read(here(stec_dir, 'mangatu_coregistered_LA', 'mangatu_1960_ero_feat_nztm.shp'))
+g70 = st_read(here(stec_dir, 'mangatu_coregistered_LA', 'mangatu_1970_ero_feat_nztm.shp'))
 g88 = st_read(here(stec_dir, 'Mangatu_feature_extraction', 'mangatu_1988_ero_feat_nztm.shp'))
-gully_centroid = g88 %>%
+g97_1 = st_read(here(stec_dir, 'Mangatu_feature_extraction', 'gullies_97_tm.shp'))
+g97_2 = st_read(here(stec_dir, 'Mangatu_feature_extraction', 'gullies_97_f_tm.shp'))
+
+#+ Combine, echo = T
+# Establish some common variables
+eros_marden2014 = list(g39, g60, g70, g88)
+year_marden2014 = c(1939, 1960, 1970, 1988)
+# Create function to harmonize datasets
+prepare_for_merge = function(x, y, colnames) {
+  z = mutate(x, year = y)
+  if (!is.null(colnames)) {
+    names(z) = c(colnames, 'year')
+  }
+  z
+}
+# Merge data
+eros_ = purrr::map2(
+  eros_marden2014,
+  year_marden2014,
+  prepare_for_merge,
+  colnames = names(g39)
+)
+eros_merge = do.call(rbind, eros_) %>% 
+  mutate_if(is.character, factor)
+
+#+ Filter
+gullies_marden2014 = eros_merge %>% 
   filter(str_detect(eros_feat_, "gully")) %>% 
-  mutate(geometry = st_centroid(geometry))
+  select(year) 
 
-gully_poly = g88 %>%
-  filter(str_detect(eros_feat_, "gully"), Shape_Area < 1e5) 
+#' Apply same process for data from Marden et al. 2012
+#+ Combine2
+eros_marden2012 = list(g57, g97_1, g97_2)
+year_marden2012 = c(1957, 1997, 1997)
+l_ = lapply(eros_marden2012, st_cast, "POLYGON")
+l_0 = lapply(l_, select, geometry)
+l_1 = purrr::map2(
+  l_0,
+  year_marden2012,
+  prepare_for_merge,
+  colnames = NULL
+)
+gullies_marden2012 = do.call(rbind, l_1) %>% 
+  mutate_if(is.character, factor) %>% 
+  st_transform(crs = st_crs(gullies_marden2014))
 
-set.seed(57)
-gully_point = gully_poly %>%
-  st_buffer(-5) %>% 
-  st_sample(size = 500, type = "random")
+#' Merge both sources
+#+ connect
+gullies = rbind(gullies_marden2012, gullies_marden2014)
 
-sa = st_read(here(stec_dir, "Mangatu_feature_extraction", "mangatu_study_area.shp"))
-sa_proj = sa %>%
-  st_transform(crs = st_crs(g88)) 
+#' Export data
+#+ export, eval = F
+st_write(gullies, here("deepl_sample_preparation/gullies.shp"), delete_dsn = T)
 
-sa_border = sa_proj %>% 
-  st_buffer(-1000)
-
-erosion_features_buf = g88 %>% 
-  st_union() %>% 
-  st_buffer(units::set_units(100, "m"))
-
-no_feature = st_difference(sa_border, erosion_features_buf)
-
-set.seed(837)
-control_samples = no_feature %>% 
-  st_sample(500) %>% 
-  st_sf() %>% 
-  transmute(class = "control", classvalue = 0)
-
-gully = gully_point %>% 
-  st_sf() %>% 
-  transmute(class = "gully", classvalue = 1)
-
-
-samples = rbind(gully, control_samples) 
-st_write(samples, "deepl_sample_preparation/sample_point_features.shp", delete_dsn = T)
-st_write(gully, "deepl_sample_preparation/gully_point_features.shp", delete_dsn = T)
-
-# library(tmap)
-# tmap_mode('view')
-# tm_shape(gully_poly) +
-#   tm_polygons(col = "red") +
-#   tm_shape(samples) +
-#   tm_dots(col = "class")
+#+ render, eval = F, include = F
+o = knitr::spin('exploration/esda.R', knit = FALSE)
+rmarkdown::render(o)
