2608228480ff59fbc2b312b1fa54ad3d825b5cd0
loreabad6 <lore.abad6@gmail.com>
2021-10-21 14:29:59 +0200

    Exploring results and validation

---
+++ exploration/deepl3.R
@@ -0,0 +1,91 @@
+library(sf)
+library(stars)
+library(here)
+library(tidyverse)
+
+ref_dir = here("from262/gullies/dump_15082021/reference/")
+output_dir = here("from262/gullies/dump_15082021/output/")
+output_fs = list.files(
+  output_dir,
+  pattern = "^lsfctshadetridx_train.*shp$", 
+  full.names = TRUE
+)
+output_name = list.files(
+  output_dir,
+  pattern = "^lsfctshadetridx_train.*shp$", 
+  full.names = FALSE
+) %>% str_extract("pad[0-9]+") %>% 
+  str_extract("[0-9]+")
+
+read_id = function(x, name) {
+  read_sf(x) %>% 
+    mutate(pad = as.numeric(name))
+}
+detectedObjects = map2(output_fs, output_name, read_id) %>% 
+  bind_rows() %>% 
+  arrange(Confidence)
+
+train = here(ref_dir, "gullies_train.shp") %>% 
+  read_sf() %>% 
+  st_transform(st_crs(detectedObjects))
+test = here(ref_dir, "gullies_test.shp") %>% 
+  read_sf() %>% 
+  st_transform(st_crs(detectedObjects))
+
+
+detectedObjects = detectedObjects %>% 
+  mutate(area = as.numeric(st_area(geometry)))
+
+detectedObjectsFiltered = detectedObjects %>% 
+  filter(area > 2000 | Confidence > 80)
+
+
+ggplot(detectedObjectsFiltered) +
+  geom_histogram(aes(Confidence)) +
+  facet_wrap(~pad)
+
+
+ggplot(detectedObjectsFiltered) +
+  aes(Confidence, area) +
+  geom_point() +
+  geom_smooth(method = "loess") +
+  scale_y_continuous(trans = "log10") +
+  facet_wrap(~pad) 
+
+ggplot(detectedObjectsFiltered) +
+  geom_sf(aes(fill = Confidence), col = NA) +
+  scale_fill_viridis_c() +
+  theme_void() +
+  theme(panel.background = element_rect(fill = "black")) 
+ggplot(filter(detectedObjects, area < 2500)) +
+  geom_sf(aes(fill = Confidence), col = NA) +
+  scale_fill_viridis_c() +
+  theme_void() +
+  theme(panel.background = element_rect(fill = "black")) 
+
+library(mapview)
+
+mapview(detectedObjectsFiltered, zcol = "Confidence") +
+  mapview(train, col.regions = 'red') +
+  mapview(test, col.regions = 'orange')
+
+test %>% 
+  st_filter(detectedObjectsFiltered, .predicate = st_intersects)
+
+detectedObjectsFilteredInt = detectedObjectsFiltered %>% 
+  rowwise() %>% 
+  mutate(
+    areaInt = st_intersection(geometry, test$geometry) %>%
+      st_area() %>% sum()
+  )
+
+save(detectedObjectsFilteredInt, file = "backup_validation_gullies.RData")
+
+
+t = detectedObjectsFilteredInt %>% 
+  mutate(intPerc = as.numeric(areaInt)/area)
+
+t %>% 
+  filter(intPerc > 0, intPerc <= 1) %>% 
+  ggplot() +
+  geom_point(aes(intPerc, Confidence))
