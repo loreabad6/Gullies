2fa27ce4fab0e032e045b20e2c0a39022d363e24
loreabad6 <lore.abad6@gmail.com>
2023-06-01 16:57:45 +0200

    Terrain derivatives for 3x3 and 5x5

--- pre-processing/terrain_derivatives.R
+++ pre-processing/terrain_derivatives.R
@@ -18,6 +18,7 @@
 knitr::opts_chunk$set(echo = TRUE, warning = F, message = F, out.width = "100%")
 
 #+ Libraries
+library(RSAGA)
 library(here)
 # remotes::install_github('loreabad6/terrain')
 library(terrain)
@@ -45,7 +46,7 @@ elev_to_sgrd(
 saga_path = here('..', 'Earthflows_R/software/saga-7.6.1_x64')
 saga_env = init_saga(saga_path)
 
-#' ## Calculate derivatives
+#' ## Calculate derivatives for original resolution
 #+ demset, eval = F
 dem = here(out, paste0(prefix, 'dem.sgrd'))
 
@@ -57,6 +58,158 @@ elev_to_morphometry(
   envir = saga_env,
   cplan = T,
   cprof = T,
+  ccros = T,
+  clong = T,
+  ddgrd = T,
+  mbidx = T,
+  slope = T,
+  textu = T,
+  tridx = T
+)
+
+elev_to_lighting(
+  elev_sgrd = dem,
+  out_dir = out,
+  prefix = prefix,
+  envir = saga_env,
+  shade = T
+)
+
+elev_to_terrain_analysis(
+  elev_sgrd = dem,
+  out_dir = out,
+  prefix = prefix,
+  envir = saga_env,
+  lsfct = T,
+  flow = T,
+  spcar = T,
+  twidx = T
+)
+
+slope = here(out, paste0(prefix, 'slope.sgrd'))
+spcar = here(out, paste0(prefix, 'spcar.sgrd'))
+flow = here(out, paste0(prefix, 'flow.sgrd'))
+
+elev_to_hidrology(
+  elev_sgrd = dem,
+  slope_sgrd = slope,
+  spcar_sgrd = spcar,
+  out_dir = out,
+  prefix = prefix,
+  envir = saga_env,
+  spidx = T
+)
+
+elev_to_channel(
+  elev_sgrd = dem,
+  flow_sgrd = flow,
+  out_dir = out,
+  prefix = prefix,
+  envir = saga_env,
+  chnet = TRUE,
+  vdcnw = TRUE,
+  init_value = 500,
+  chnet_shp = TRUE
+)
+
+#' ## Calculate derivatives for 3x3 resolution
+prefix = "mangatu_3x3_"
+
+rsaga.filter.simple(
+  in.grid = dem, 
+  out.grid = here(out, paste0(prefix, "dem.sgrd")),
+  method = 'smooth', radius = 1, mode = 'square', 
+  env = saga_env
+)
+
+#+ demset3, eval = F
+dem = here(out, paste0(prefix, "dem.sgrd"))
+
+#+ calc3, eval = F
+elev_to_morphometry(
+  elev_sgrd = dem,
+  out_dir = out,
+  prefix = prefix,
+  envir = saga_env,
+  cplan = T,
+  cprof = T,
+  ccros = T,
+  clong = T,
+  ddgrd = T,
+  mbidx = T,
+  slope = T,
+  textu = T,
+  tridx = T
+)
+
+elev_to_lighting(
+  elev_sgrd = dem,
+  out_dir = out,
+  prefix = prefix,
+  envir = saga_env,
+  shade = T
+)
+
+elev_to_terrain_analysis(
+  elev_sgrd = dem,
+  out_dir = out,
+  prefix = prefix,
+  envir = saga_env,
+  lsfct = T,
+  flow = T,
+  spcar = T,
+  twidx = T
+)
+
+slope = here(out, paste0(prefix, 'slope.sgrd'))
+spcar = here(out, paste0(prefix, 'spcar.sgrd'))
+flow = here(out, paste0(prefix, 'flow.sgrd'))
+
+elev_to_hidrology(
+  elev_sgrd = dem,
+  slope_sgrd = slope,
+  spcar_sgrd = spcar,
+  out_dir = out,
+  prefix = prefix,
+  envir = saga_env,
+  spidx = T
+)
+
+elev_to_channel(
+  elev_sgrd = dem,
+  flow_sgrd = flow,
+  out_dir = out,
+  prefix = prefix,
+  envir = saga_env,
+  chnet = TRUE,
+  vdcnw = TRUE,
+  init_value = 500,
+  chnet_shp = TRUE
+)
+
+#' ## Calculate derivatives for 5x5 resolution
+prefix = "mangatu_5x5_"
+
+rsaga.filter.simple(
+  in.grid = dem, 
+  out.grid = here(out, paste0(prefix, "dem.sgrd")),
+  method = 'smooth', radius = 2, mode = 'square', 
+  env = saga_env
+)
+
+#+ demset5, eval = F
+dem = here(out, paste0(prefix, "dem.sgrd"))
+
+#+ calc5, eval = F
+elev_to_morphometry(
+  elev_sgrd = dem,
+  out_dir = out,
+  prefix = prefix,
+  envir = saga_env,
+  cplan = T,
+  cprof = T,
+  clong = T,
+  ccros = T,
   ddgrd = T,
   mbidx = T,
   slope = T,
@@ -142,3 +295,49 @@ knitr::include_graphics("../exploration/terrain_derivatives.png")
 #+ render, eval = F, include = F
 o = knitr::spin('pre-processing/terrain_derivatives.R', knit = FALSE)
 rmarkdown::render(o)
+
+file_names_sdat = list.files(
+  path = out,
+  pattern = ".sdat$",
+  full.names = F
+)
+file_names_tif = stringr::str_replace(
+  file_names_sdat,
+  ".sdat$",
+  ".tif"
+)
+
+existing_files = c(
+  strex::str_after_first(
+    list.files(
+      path = out,
+      pattern = ".tif$",
+      full.names = F,
+      recursive = T
+    ),
+    '/'),
+  list.files(
+    path = out,
+    pattern = ".tif$",
+    full.names = F
+  )
+)
+existing_files = existing_files[!is.na(existing_files)]
+
+files_to_translate = stringr::str_replace(
+  file_names_tif[!(file_names_tif %in% existing_files)],
+  ".tif", '.sdat'
+)
+
+files_for_translation = here(out, files_to_translate)
+
+params = list(
+  infiles = files_for_translation,
+  outdir = out,
+  outsuffix = ".tif",
+  # a_srs = if(!is.null(out_crs)) out_crs,
+  verbose = TRUE
+)
+
+do.call(gdalUtils::batch_gdal_translate, params[lengths(params) > 0])
+
